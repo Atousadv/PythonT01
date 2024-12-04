3a1ed16fa279f5ecbd29a145b59b5bf9d66d8986
Atousadv <atousa.davoodian@gmail.com>
2024-12-04 17:17:22 +0100

    CalT01 Dec2024

--- DateLibraryEx.py
+++ DateLibraryEx.py
@@ -6,3 +6,9 @@
 # currentDate=int(input("what is current date?"))
 # x=int(currentDate-age)
 # print(x)
+
+from datetime import datetime
+date=input("insert a date:")
+date_object=datetime.strptime(date,"%Y-%m-%d")
+week_day=date_object.strftime("%A")
+print(week_day)
